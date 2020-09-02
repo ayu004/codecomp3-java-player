60f41014a62dac88e7690e135e79e61b6ebe8967
ayu004 <sd.aayush@gmail.com>
2020-09-02 21:37:34 +0530

    Initial Commit

---
+++ src/main/java/com/gcp/codecomp/controller/Controller.java
@@ -0,0 +1,21 @@
+package com.gcp.codecomp.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin
+public class Controller {
+	
+	@GetMapping("/")
+	public void get() {
+		System.out.println("Hit at the root API / received");
+	}
+	
+	@GetMapping("/hit")
+	public void getTotal() {
+		System.out.println("Hit at the API /hit received");
+	}
+
+}
